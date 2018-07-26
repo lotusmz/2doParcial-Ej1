c66219141d955f410e9e74cbba6e0a8fce5ae34b
lotusmz <mary_zap@hotmail.com>
2018-07-26 10:32:23 -0600

    refactorizacion y test

--- src/cr/ac/cenfotec/CribaEratostenes/CribaEratostenes.java
+++ src/cr/ac/cenfotec/CribaEratostenes/CribaEratostenes.java
@@ -14,24 +14,9 @@ public class CribaEratostenes {
 			for (i = 0; i < dim; i++){
 				primo[i] = true;
 			}
+		
+			primo = obtenerListaPrimo(primo, dim);
 			
-			//Se declara los primero dos indices del arreglo se como No Primos
-			primo[0] = primo[1] = false;
-			
-			//Si i es menor a la raiz cuadrada de dim + 1 se entra en la condición
-			for (i = 2; i< Math.sqrt(dim) + 1; i++){
-				
-				//i está marcado como primo se entra en la condición
-				if (primo[i]){
-					
-					//Define a todos los multiplos de 2 como No primos
-					for (j = 2*i; j < dim; j+=i){
-
-						primo[j] = false;
-
-					}
-				}
-			}
 			int cuenta = 0;
 			//Cuenta todos los numeros primos obtenidos
 			for (i = 0; i < dim; i++){
@@ -51,6 +36,31 @@ public class CribaEratostenes {
 		} else {
 			return new int[0];
 		}
-	}		
+	}
+	
+	public static boolean[] obtenerListaPrimo(boolean[] pPrimo, int pDim) {
+		
+		int i,j ;
+		
+		//Se declara los primero dos indices del arreglo se como No Primos
+		pPrimo[0] = pPrimo[1] = false;
+		
+		//Si i es menor a la raiz cuadrada de dim + 1 se entra en la condición
+		for (i = 2; i< Math.sqrt(pDim) + 1; i++){
+			
+			//i está marcado como primo se entra en la condición
+			if (pPrimo[i]){
+				
+				//Define los # que son multiplos del # primo como no primos
+				for (j = 2*i; j < pDim; j+=i){
+
+					pPrimo[j] = false;
+
+				}
+			}
+		}
+		
+		return pPrimo;
+	}
 		
 }
--- src/cr/ac/cenfotec/CribaEratostenesTest/CribaEratostenesTest.java
+++ src/cr/ac/cenfotec/CribaEratostenesTest/CribaEratostenesTest.java
@@ -8,12 +8,22 @@ import cr.ac.cenfotec.CribaEratostenes.CribaEratostenes;
 
 public class CribaEratostenesTest {
 	CribaEratostenes nuevaCriba = new CribaEratostenes();
+	boolean[] primos;
+	int dim = 21;
 	
 	@Test
 	public void testObtenerPrimos() {
 		
+		primos = new boolean[dim];
 		
-		fail("Not yet implemented");
+		for (int i = 0; i < dim; i++){
+			primos[i] = true;
+		}		
+				
+		primos = CribaEratostenes.obtenerListaPrimo(primos, dim);
+		assertEquals(false, primos[0]);
+		assertEquals(false, primos[1]);
+		assertEquals(true, primos[2]);
 	}
 
 }
